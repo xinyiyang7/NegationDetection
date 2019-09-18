935e86f9f0b0d08006b9355a01750d2cecb5a172
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-18 04:09:27 -0700

    coding

---
+++ train_negation_rule_based.py
@@ -0,0 +1,12 @@
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+def negation_detection(strr):
+    wordlist = word_tokenize(strr)
+    word_pos_list = pos_tag(wordlist)
+    print('wordlist:', wordlist)
+    print('word_pos_list:', word_pos_list)
+
+
+if __name__ == "__main__":
+    negation_detection('we do not like the dog.')
