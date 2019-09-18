8cfc9189a453c7c12a42c424958956fd2468d4a0
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-18 04:35:56 -0700

    coding

--- train_negation_rule_based.py
+++ train_negation_rule_based.py
@@ -4,11 +4,50 @@ nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 
+NEGATION_ADVERBS = ["no", "without", "nil","not", "n't", "never", "none", "neith", "nor", "non"]
+NEGATION_VERBS = ["deny", "reject", "refuse", "subside", "retract", "non"]
+
+def scope_detection(word_pos_list, neg_id):
+    indictors = []
+    for pair in word_pos_list:
+        if pair[1] in set(['JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS', 'TO', 'IN', 'VB','VBD','VBG','VBN','VBP','VBZ']):
+            indictors.append(1)
+        else:
+            indictors.append(0)
+
+    left_most = neg_id-1
+    while indictors[left_most] !=1:
+        left_most-=1
+    right_most = neg_id+1
+    while indictors[right_most] !=1:
+        right_most+=1
+
+    scope_list = []
+    if neg_id - left_most > right_most - neg_id:
+        for i in range(right_most, len(word_pos_list)):
+            if word_pos_list[right_most][1] == 1:
+                scope_list.append(word_pos_list[right_most][0])
+    else:
+        for i in range(left_most, -1, -1):
+            if word_pos_list[left_most][1] == 1:
+                scope_list.append(word_pos_list[left_most][0])
+    print('scope:', scope_list)
+
 def negation_detection(strr):
     wordlist = word_tokenize(strr)
     word_pos_list = pos_tag(wordlist)
-    print('wordlist:', wordlist)
-    print('word_pos_list:', word_pos_list)
+    # print('wordlist:', wordlist)
+    # print('word_pos_list:', word_pos_list)
+    assert len(wordlist) ==  len(word_pos_list)
+    for id, pair in enumerate(word_pos_list):
+        word = pair[0]
+        pos = pair[1]
+        if word in set(NEGATION_ADVERBS) or word in set(NEGATION_VERBS) or word[:2] == 'un' or word[:3]=='dis':
+            print('negate word:', word)
+            scope_detection(word_pos_list, id)
+
+
+
 
 
 if __name__ == "__main__":
